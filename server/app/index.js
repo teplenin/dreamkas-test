21e93759a8829760382915210a5c3afd37158f9b
Dmitry Teplenin <teplenin@trend-spb.ru>
2017-08-16 09:34:47 +0300

    Добавлено: CORS (#4)

--- server/app/index.js
+++ server/app/index.js
@@ -1,4 +1,5 @@
 import Koa from 'koa';
+import cors from 'koa2-cors';
 
 import { routesMiddleware } from './middleware/routes';
 import errorMiddleware from './middleware/error';
@@ -7,13 +8,16 @@ const app = new Koa();
 const router = routesMiddleware();
 
 app.use(errorMiddleware);
+
+app.use(cors({ origin: '*' }));
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.use(function (ctx, next) {
     ctx.statusCode = 404;
     ctx.body = { status: 'error', message: 'Not found' }
-})
+});
 
 app.listen(3000, function () {
     console.log('Server listening at port 3000');
